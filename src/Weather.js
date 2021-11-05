95b6ce3983403f3d86a8713fd5eb66fe93a3747c
cdonal91 <89680346+cdonal91@users.noreply.github.com>
2021-11-05 22:42:17 +1100

    Adding in daily forecast API

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -9,6 +10,7 @@ export default function Weather(props) {
   function handleResponse(response) {
     setWeatherInfo({
       ready: true,
+      coordinates: response.data.coordinates,
       temperature: response.data.main.temp,
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
@@ -58,6 +60,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherInfo} />
+        <WeatherForecast coordinates={weatherInfo.coord} />
       </div>
     );
   } else {
